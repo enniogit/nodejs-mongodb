fb72ba519ea6a251ed7423e0204309de2010b67a
Ennio Picarelli <ennio_picarelli@it.ibm.com@Ennios-Air.station>
2020-04-09 07:01:54 +0200

    ora

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 const filter = {
   'name': new RegExp('paper')
 };
-var url = "mongodb://ennio:ennio@172.30.45.178:27017/sampledb";
+var url = "mongodb://mongouser:mongouser@172.30.45.178:27017/sampledb";
 console.log(url);
 MongoClient.connect(url, (err, database) => {
   if (err) return console.log(err);
@@ -36,53 +36,3 @@ app.post('/insert', (req, res) => {
     res.redirect('/')
   })
 })
-/*
-app.listen(3000, function() {
-  console.log('listening on 3000')
-})
-*/
-/*
-var risposta = MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  var dbo = db.db("sampledb");
-  return new Promise (function(resolve, reject) { dbo.collection("sampledb").find(filter).toArray(function(err, result) {
-    if (err) throw err;
-    console.log(result);
-    db.close();
-    resolve();
-  })});
-});
-Promise.all(risposta)
-.then(function() { console.log('fatto)'); })
-.catch(console.error);
-/*
-MongoClient.connect(
-  'mongodb://ennio:ennio@localhost:34000/sampledb',
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  function(connectErr, client) {
-    assert.equal(null, connectErr);
-    const coll = client.db('sampledb').collection('sampledb');
-    coll.find({}, (cmdErr, result) => {
-      assert.equal(null, cmdErr);
-      console.log(result);
-    });
-    client.close();
-  });
-  */
-/*
-var client = require('mongodb').MongoClient;
-client.connect("mongodb://ennio:ennio@localhost:34000/sampledb", function(error, sampledb) {
-   if(! error) {
-      console.log('success');
-
-   }
-});
-*/
-/*
-var http = require('http');
-var server = http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end(JSON.stringify (risultato));
-});
-server.listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');*/
